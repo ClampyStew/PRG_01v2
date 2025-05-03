58087bc8fe3c12f9a8b198d7f21fba00e322fb58
ClampyZK <clamstew13@gmail.com>
2025-05-03 09:45:30 +0800

    Proper DisplayTable script has been added

--- Week 2/DisplayTable.py
+++ Week 2/DisplayTable.py
@@ -1,32 +1,16 @@
-# print('''Number     Square      Square Root     English
-#          1                                       One
-#          2                                       Two
-#          3                                       Three
-#          4                                       Four
-#          5                                       Five''')
 
-# num=1
-# square1 = num**2
-# squareroot1 = num**(0.5)
-
-# num=2
-# square2 = num**2
-# squareroot2 = num**(0.5)
-
-# num=3
-# square3 = num**2
-# squareroot3 = num**(0.5)
-
-# num=4
-# square4 = num**2
-# squareroot4 = num**(0.5)
-
-# num=5
-# square5 = num**2
-# squareroot5 = num**(0.5)
-
-
+sq2 = 2**2
+sr2 = 2**(1/2)
+sq3 = 3**2
+sr3 = 3**(1/2)
+sq4 = 4**2
+sr4 = 4**(1/2)
+sq5 = 5**2
+sr5 = 5**(1/2)
 
 print(f'{'Number'}  {'Square'}  {'Square root'}  {'English'}')
 print(f'{1:>6}  {1:^6}  {1:^11.2f}  {"One":>7s}')
-print(f'{2:>6}  {2:^6}  {2:}  {"Two":>7s}')
+print(f'{2:>6}  {sq2:^6}  {sr2:^11.2f}  {"Two":>7s}')
+print(f'{3:>6}  {sq3:^6}  {sr3:^11.2f}  {"Three":>7s}')
+print(f'{4:>6}  {sq4:^6}  {sr4:^11.2f}  {"Four":>7s}')
+print(f'{5:>6}  {sq5:^6}  {sr5:^11.2f}  {"Five":>7s}')
